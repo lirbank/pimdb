070a1ef287e92877f960d34d9cdf51be9af869c2
Mikael Lirbank <mikael@lirbank.com>
2024-11-17 16:59:52 -0800

    Update sorted.test.ts

--- src/indexes/sorted.test.ts
+++ src/indexes/sorted.test.ts
@@ -7,126 +7,6 @@ interface TestDoc {
   age?: number;
 }
 
-// describe("stable results", () => {
-//   let index: PimSortedIndex<TestDoc>;
-
-//   beforeEach(() => {
-//     index = new PimSortedIndex<TestDoc>("name");
-//     const docs = [
-//       { id: "b", name: "aaa" },
-//       { id: "c", name: "aaa" },
-//       { id: "a", name: "aaa" },
-//     ];
-//     docs.forEach((doc) => index.insert(doc));
-//   });
-
-//   test("returns all documents in stable order when no query", () => {
-//     expect(index.find()).toEqual([
-//       { id: "a", name: "aaa" },
-//       { id: "b", name: "aaa" },
-//       { id: "c", name: "aaa" },
-//     ]);
-//   });
-
-//   test("returns matching documents in stable order when queried", () => {
-//     expect(index.find("aaa")).toEqual([
-//       { id: "a", name: "aaa" },
-//       { id: "b", name: "aaa" },
-//       { id: "c", name: "aaa" },
-//     ]);
-//   });
-// });
-
-test("returns results sorted by id regardless of insertion order", () => {
-  const index = new PimSortedIndex<TestDoc>("name");
-
-  // Insert documents in arbitrary order
-  index.insert({ id: "3", name: "ccc" });
-  index.insert({ id: "5", name: "ccccc" });
-  index.insert({ id: "7", name: "bbb" });
-  index.insert({ id: "6", name: "bbb" });
-  index.insert({ id: "0", name: "ccc" });
-  index.insert({ id: "4", name: "aaa" });
-  index.insert({ id: "1", name: "aaa" });
-  index.insert({ id: "2", name: "aaa" });
-
-  // Matching documents secondarily sorted by id
-  expect(index.find("aaa")).toEqual([
-    { id: "1", name: "aaa" },
-    { id: "2", name: "aaa" },
-    { id: "4", name: "aaa" },
-  ]);
-
-  // Matching documents secondarily sorted by id
-  expect(index.find("bbb")).toEqual([
-    { id: "6", name: "bbb" },
-    { id: "7", name: "bbb" },
-  ]);
-
-  // Matching documents secondarily sorted by id
-  expect(index.find("ccc")).toEqual([
-    { id: "0", name: "ccc" },
-    { id: "3", name: "ccc" },
-  ]);
-
-  // All documents secondarily sorted by id
-  expect(index.find()).toEqual([
-    { id: "1", name: "aaa" },
-    { id: "2", name: "aaa" },
-    { id: "4", name: "aaa" },
-    { id: "6", name: "bbb" },
-    { id: "7", name: "bbb" },
-    { id: "0", name: "ccc" },
-    { id: "3", name: "ccc" },
-    { id: "5", name: "ccccc" },
-  ]);
-});
-
-test("returns results sorted by id regardless of insertion order", () => {
-  const index = new PimSortedIndex<TestDoc>("name");
-
-  // Insert documents in arbitrary order
-  index.insert({ id: "3", name: "ccc" });
-  index.insert({ id: "5", name: "ccccc" });
-  index.insert({ id: "7", name: "bbb" });
-  index.insert({ id: "6", name: "bbb" });
-  index.insert({ id: "0", name: "ccc" });
-  index.insert({ id: "4", name: "aaa" });
-  index.insert({ id: "1", name: "aaa" });
-  index.insert({ id: "2", name: "aaa" });
-
-  // Matching documents secondarily sorted by id
-  expect(index.findInRange({ gte: "aaa", lte: "aaa" })).toEqual([
-    { id: "1", name: "aaa" },
-    { id: "2", name: "aaa" },
-    { id: "4", name: "aaa" },
-  ]);
-
-  // Matching documents secondarily sorted by id
-  expect(index.findInRange({ gte: "bbb", lte: "bbb" })).toEqual([
-    { id: "6", name: "bbb" },
-    { id: "7", name: "bbb" },
-  ]);
-
-  // Matching documents secondarily sorted by id
-  expect(index.findInRange({ gte: "ccc", lte: "ccc" })).toEqual([
-    { id: "0", name: "ccc" },
-    { id: "3", name: "ccc" },
-  ]);
-
-  // All documents secondarily sorted by id
-  expect(index.find()).toEqual([
-    { id: "1", name: "aaa" },
-    { id: "2", name: "aaa" },
-    { id: "4", name: "aaa" },
-    { id: "6", name: "bbb" },
-    { id: "7", name: "bbb" },
-    { id: "0", name: "ccc" },
-    { id: "3", name: "ccc" },
-    { id: "5", name: "ccccc" },
-  ]);
-});
-
 test("find with upper and lower case", () => {
   const index = new PimSortedIndex<TestDoc>("name");
 
@@ -151,20 +31,94 @@ test("find with upper and lower case", () => {
     { id: "c", name: "yyy" },
   ]);
 
-  // Query for empty string returns documents with empty name
-  expect(index.find("")).toEqual([
-    { id: "d", name: "" },
-    { id: "h", name: "" },
-  ]);
-
-  // Query for "aaa" returns documents with "aaa" name
-  expect(index.find("aaa")).toEqual([
-    { id: "e", name: "aaa" },
-    { id: "g", name: "aaa" },
-  ]);
-
   // Query for unknown value returns empty array
   expect(index.find("unknown")).toEqual([]);
+});
+
+/**
+ * find
+ */
+describe("find", () => {
+  let index: PimSortedIndex<TestDoc>;
+
+  beforeEach(() => {
+    // Reset index before each test
+    index = new PimSortedIndex<TestDoc>("name");
+    // Insert documents in random order to verify sorting
+    [
+      { id: "3", name: "ccc" },
+      { id: "5", name: "ccccc" },
+      { id: "7", name: "bbb" },
+      { id: "6", name: "bbb" },
+      { id: "0", name: "ccc" },
+      { id: "9", name: "" },
+      { id: "11", name: "BBB" },
+      { id: "10", name: "AAA" },
+      { id: "8", name: "" },
+      { id: "4", name: "aaa" },
+      { id: "1", name: "aaa" },
+      { id: "2", name: "aaa" },
+    ].forEach((doc) => index.insert(doc));
+  });
+
+  test("undefined query returns all documents, secondarily sorted by id", () => {
+    // All documents secondarily sorted by id
+    expect(index.find()).toEqual([
+      { id: "8", name: "" },
+      { id: "9", name: "" },
+      { id: "10", name: "AAA" },
+      { id: "11", name: "BBB" },
+      { id: "1", name: "aaa" },
+      { id: "2", name: "aaa" },
+      { id: "4", name: "aaa" },
+      { id: "6", name: "bbb" },
+      { id: "7", name: "bbb" },
+      { id: "0", name: "ccc" },
+      { id: "3", name: "ccc" },
+      { id: "5", name: "ccccc" },
+    ]);
+  });
+
+  test("returns 'AAA' documents sorted by id", () => {
+    expect(index.find("AAA")).toEqual([{ id: "10", name: "AAA" }]);
+  });
+
+  test("returns 'BBB' documents sorted by id", () => {
+    expect(index.find("BBB")).toEqual([{ id: "11", name: "BBB" }]);
+  });
+
+  test("returns 'aaa' documents sorted by id", () => {
+    expect(index.find("aaa")).toEqual([
+      { id: "1", name: "aaa" },
+      { id: "2", name: "aaa" },
+      { id: "4", name: "aaa" },
+    ]);
+  });
+
+  test("returns 'bbb' documents sorted by id", () => {
+    expect(index.find("bbb")).toEqual([
+      { id: "6", name: "bbb" },
+      { id: "7", name: "bbb" },
+    ]);
+  });
+
+  test("returns 'ccc' documents sorted by id", () => {
+    expect(index.find("ccc")).toEqual([
+      { id: "0", name: "ccc" },
+      { id: "3", name: "ccc" },
+    ]);
+  });
+
+  test("returns '' documents sorted by id", () => {
+    expect(index.find("")).toEqual([
+      { id: "8", name: "" },
+      { id: "9", name: "" },
+    ]);
+  });
+
+  test("returns empty array when no match", () => {
+    expect(index.find("unknown")).toEqual([]);
+  });
 });
 
 /**
@@ -174,17 +128,66 @@ describe("findInRange", () => {
   let index: PimSortedIndex<TestDoc>;
 
   beforeEach(() => {
+    // Reset index before each test
     index = new PimSortedIndex<TestDoc>("name");
     // Insert documents in random order to verify sorting
     [
+      { id: "3", name: "ccc" },
+      { id: "5", name: "ccccc" },
+      { id: "7", name: "bbb" },
+      { id: "6", name: "bbb" },
+      { id: "0", name: "ccc" },
+      { id: "9", name: "" },
+      { id: "11", name: "BBB" },
+      { id: "10", name: "AAA" },
+      { id: "8", name: "" },
+      { id: "4", name: "aaa" },
       { id: "1", name: "aaa" },
       { id: "2", name: "aaa" },
+    ].forEach((doc) => index.insert(doc));
+  });
+
+  test("undefined bounds returns all documents, secondarily sorted by id", () => {
+    const expected = [
+      { id: "8", name: "" },
+      { id: "9", name: "" },
+      { id: "10", name: "AAA" },
+      { id: "11", name: "BBB" },
+      { id: "1", name: "aaa" },
+      { id: "2", name: "aaa" },
+      { id: "4", name: "aaa" },
       { id: "6", name: "bbb" },
       { id: "7", name: "bbb" },
       { id: "0", name: "ccc" },
       { id: "3", name: "ccc" },
-      { id: "5", name: "ddd" },
-    ].forEach((doc) => index.insert(doc));
+      { id: "5", name: "ccccc" },
+    ];
+
+    // Empty object or undefined returns all documents
+    expect(index.findInRange({})).toEqual(expected);
+    expect(index.findInRange()).toEqual(expected);
+  });
+
+  test("returns 'aaa' documents sorted by id", () => {
+    expect(index.findInRange({ gte: "aaa", lte: "aaa" })).toEqual([
+      { id: "1", name: "aaa" },
+      { id: "2", name: "aaa" },
+      { id: "4", name: "aaa" },
+    ]);
+  });
+
+  test("returns 'bbb' documents sorted by id", () => {
+    expect(index.findInRange({ gte: "bbb", lte: "bbb" })).toEqual([
+      { id: "6", name: "bbb" },
+      { id: "7", name: "bbb" },
+    ]);
+  });
+
+  test("returns 'ccc' documents sorted by id", () => {
+    expect(index.findInRange({ gte: "ccc", lte: "ccc" })).toEqual([
+      { id: "0", name: "ccc" },
+      { id: "3", name: "ccc" },
+    ]);
   });
 
   test("returns empty array when range is after all values", () => {
@@ -213,46 +216,27 @@ describe("findInRange", () => {
     ]);
   });
 
-  test("handles undefined start", () => {
+  test("handles undefined lower bound", () => {
     // Should include everything <= "bbb"
     expect(index.findInRange({ lte: "bbb" })).toEqual([
+      { id: "8", name: "" },
+      { id: "9", name: "" },
+      { id: "10", name: "AAA" },
+      { id: "11", name: "BBB" },
       { id: "1", name: "aaa" },
       { id: "2", name: "aaa" },
+      { id: "4", name: "aaa" },
       { id: "6", name: "bbb" },
       { id: "7", name: "bbb" },
     ]);
   });
 
-  test("handles undefined end", () => {
+  test("handles undefined upper bound", () => {
     // Should include everything >= "ccc"
     expect(index.findInRange({ gte: "ccc" })).toEqual([
       { id: "0", name: "ccc" },
       { id: "3", name: "ccc" },
-      { id: "5", name: "ddd" },
-    ]);
-  });
-
-  test("handles both undefined bounds", () => {
-    // Should return all documents
-    expect(index.findInRange({})).toEqual([
-      { id: "1", name: "aaa" },
-      { id: "2", name: "aaa" },
-      { id: "6", name: "bbb" },
-      { id: "7", name: "bbb" },
-      { id: "0", name: "ccc" },
-      { id: "3", name: "ccc" },
-      { id: "5", name: "ddd" },
-    ]);
-
-    // Should return all documents
-    expect(index.findInRange()).toEqual([
-      { id: "1", name: "aaa" },
-      { id: "2", name: "aaa" },
-      { id: "6", name: "bbb" },
-      { id: "7", name: "bbb" },
-      { id: "0", name: "ccc" },
-      { id: "3", name: "ccc" },
-      { id: "5", name: "ddd" },
+      { id: "5", name: "ccccc" },
     ]);
   });
 });
