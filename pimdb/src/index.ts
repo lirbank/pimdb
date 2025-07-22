3a4020a3ce7a5f52d7c99731b87e147ee80dcf93
Mikael Lirbank <mikael@lirbank.com>
2025-07-21 19:02:19 -0700

    rm

--- pimdb/src/index.ts
+++ pimdb/src/index.ts
@@ -2,5 +2,3 @@ export * from "./pimdb";
 export * from "./indexes/primary";
 export * from "./indexes/sorted";
 export * from "./indexes/substring";
-export * from "./collection-builder";
-export * from "./index-factories";
